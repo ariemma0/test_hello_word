677550acd16594f7bf34363c5ca459cd7474e9a8
ariemma0 <aariemma@ramapo.edu>
2016-11-15 12:18:03 -0500

    new http

--- http.js
+++ http.js
@@ -1,6 +1,6 @@
-var http = require('http');
 
-http.createServer(function (req, res) {
+
+
   res.writeHead(200, {'Content-Type': 'text/html'});
 
   var html = "<!doctype html>" + 
@@ -14,7 +14,7 @@ http.createServer(function (req, res) {
   			 "\n</html>";
 
   res.end(html);
-}).listen(3000, '127.0.0.1');
 
 
-console.log('Server running at http://127.0.0.1:3000/');
+
+console.log('Server running at ');
